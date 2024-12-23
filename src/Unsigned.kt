b66ad78350c336c2677efe6c1916e0ee80da7f0c
Naykyr Oliveira <naykyr@hotmail.com>
2024-12-23 16:02:57 -0300

    Tipos de dados Unsigned

---
+++ src/Unsigned.kt
@@ -0,0 +1,16 @@
+fun main() {
+
+    // Unsigned
+
+    println("ULong MAX ${ULong.MAX_VALUE} -MIN ${ULong.MIN_VALUE}")
+    println("UInt MAX ${UInt.MAX_VALUE} -MIN ${UInt.MIN_VALUE}")
+    println("UShort MAX ${UShort.MAX_VALUE} -MIN ${UShort.MIN_VALUE}")
+    println("UByte MAX ${UByte.MAX_VALUE} -MIN ${UByte.MIN_VALUE}")
+
+    val abc: UInt = 0u
+
+    val short: UShort = 10u
+    val byte: UByte = 10u
+    val long: ULong = 10u
+
+}
