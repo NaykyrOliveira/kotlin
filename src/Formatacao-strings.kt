cdf98e9f43c4755ff1985c7ea482b58ff8369226
Naykyr Oliveira <naykyr@hotmail.com>
2024-12-24 11:12:01 -0300

    Formatação de strings

---
+++ src/Formatacao-strings.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val nome = "Tony"
+    val sobrenome = "Stark"
+    println("Olá, $nome $sobrenome")
+
+    println("Double: ${Double.MAX_VALUE}")
+
+    val str = "textos " +
+            "muito grandes" +
+            "textos grandes "
+    println(str)
+}
