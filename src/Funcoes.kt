a3f97f4efc30dd4da715e5eea56ec4b7f887d6e1
Naykyr Oliveira <naykyr@hotmail.com>
2024-12-24 12:44:08 -0300

    Funções

---
+++ src/Funcoes.kt
@@ -0,0 +1,12 @@
+fun main() {
+    helloWorld()
+    println("A soma de 2 e 7 é: ${soma(2, 5)}")
+}
+
+fun soma(a: Int, b: Int): Int {
+    return (a + b)
+}
+
+fun helloWorld() {
+    println("Hello World")
+}
