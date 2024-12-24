2ef9ac5759eb5ad11a43b02c93ce1d27b2146bd3
Naykyr Oliveira <naykyr@hotmail.com>
2024-12-24 11:24:32 -0300

    Operadores aritméticos

---
+++ src/Operadores-aritmeticos.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var idade = 30
+    idade += 1
+    idade -= 5
+    idade = idade * 2
+    idade = idade / 2
+    println(idade)
+
+    var novaIdade = 10 % 3
+    novaIdade++
+    println(novaIdade)
+}
