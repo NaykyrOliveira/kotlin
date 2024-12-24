fae02098f227020130dbe8c1b4905d609b3360fd
Naykyr Oliveira <naykyr@hotmail.com>
2024-12-24 11:51:01 -0300

    Exercício

---
+++ src/Exercicio-1.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var nome: String = "Nome completo"
+    var texto: String
+    println(nome)
+
+    val calcado: Byte = 40
+    val pib: Long = 1869000000000
+    val populacao: Int = 211000000
+    println(calcado)
+    println(pib)
+    println(populacao)
+    println("R$ ${pib/populacao}")
+}
